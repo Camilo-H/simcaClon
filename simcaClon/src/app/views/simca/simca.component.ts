f31be6b56ffaa57b798d993bf6f702aa3158e7b1
Camilo-H <camiload@unicauca.edu.co>
2026-07-22 23:15:22 -0500

    Creación de componentes

---
+++ simcaClon/src/app/views/simca/simca.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-simca',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './simca.component.html',
+  styleUrls: ['./simca.component.css']
+})
+export class SimcaComponent {
+
+}
