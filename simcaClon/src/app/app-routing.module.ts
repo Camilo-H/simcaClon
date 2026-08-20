78e424b9f64a72c3554019994bee9e3f2b5dc840
Camilo-H <camiload@unicauca.edu.co>
2026-08-20 14:31:23 -0500

    ajuste de elementos de componente

--- simcaClon/src/app/app-routing.module.ts
+++ simcaClon/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimcaComponent } from './views/simca/simca.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'views/simca', pathMatch: 'full'},
+  {path:'views/simca', component: SimcaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
